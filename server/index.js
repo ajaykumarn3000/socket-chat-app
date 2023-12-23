20ec7dd4040fef4dac20d668337f0b61c26a3f31
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2023-12-23 14:35:21 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = 4000;
+const client_url = "http://localhost:3000"
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: client_url,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("JOIN_ROOM", (data) => {
+    socket.join(data);
+    console.log("User with id " + socket.id + " joined room: " + data);
+  });
+
+  socket.on("SEND_MESSAGE", (data) => {
+    socket.to(data.room).emit("RECEIVE_MESSAGE", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log("SERVER RUNNING ON PORT: " + port);
+});
