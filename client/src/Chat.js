9dd2424642278078b032ea356863b7888f32c305
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2023-12-23 14:33:56 +0530

    initial commit

---
+++ client/src/Chat.js
@@ -0,0 +1,69 @@
+import React, { useMemo, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time: new Date().getHours() + ":" + new Date().getMinutes(),
+      };
+      await socket.emit("SEND_MESSAGE", messageData);
+      setMessageList((prev) => [...prev, messageData]);
+      setCurrentMessage("")
+    }
+  };
+
+  useMemo(() => {
+    socket.on("RECEIVE_MESSAGE", (data) => {
+      setMessageList((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((message) => {
+            return (
+              <div
+                className="message"
+                id={username === message.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{message.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{message.time}</p>
+                    <p id="author">{message.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="write a message"
+          onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
